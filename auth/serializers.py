e235043294f4de2b0519ee6c63d690ad81550300
CFsanchezV <csv455@gmail.com>
2022-05-18 03:13:17 -0500

    return userId on register

--- auth/serializers.py
+++ auth/serializers.py
@@ -14,9 +14,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
+    id = serializers.IntegerField(read_only=True, required=False)
+
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'password', 'password2', 'email', 'first_name', 'last_name')
         extra_kwargs = {
             'first_name': {'required': False},
             'last_name': {'required': False}
